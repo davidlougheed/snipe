4d3d42caeb2f36ac4b3ec365903ff3d722c618a0
David Lougheed <david.lougheed@gmail.com>
2024-03-10 14:51:57 -0400

    refact: factor out primer set Euler diagram into component

---
+++ src/steps/discovery/PrimerSetEulerDiagram.js
@@ -0,0 +1,48 @@
+import { createVennJSAdapter, VennDiagram } from "@upsetjs/react";
+import { layout } from "@upsetjs/venn.js";
+import { lab } from "d3-color";
+
+const vennJSAdapter = createVennJSAdapter(layout);
+
+const mergeColors = (colors) => {
+    // inspired by https://upset.js.org/docs/examples/vennColored
+    switch (colors.length) {
+        case 0:
+            return undefined;
+        case 1:
+            return colors[0];
+        default: {
+            const ca = colors.reduce((acc, d) => {
+                const c = lab(d || "transparent");
+                return { l: acc.l + c.l, a: acc.a + c.a, b: acc.b + c.b };
+            }, { l: 0, a: 0, b: 0 });
+
+            const res = lab(
+                ca.l / colors.length,
+                ca.a / colors.length,
+                ca.b / colors.length,
+                0.45,
+            );
+            return res.formatRgb();
+        }
+    }
+};
+
+// Param values
+const combinations = { mergeColors };
+const style = { maxWidth: "100%", height: "auto" };
+
+const PrimerSetEulerDiagram = ({ primerSet }) => {
+    return <VennDiagram
+        layout={vennJSAdapter}
+        sets={primerSet.coveredTaxaByPrimerUpset}
+        width={990}
+        height={720}
+        theme="light"
+        padding={75}
+        combinations={combinations}
+        style={style}
+    />;
+};
+
+export default PrimerSetEulerDiagram;
--- src/steps/discovery/ResultsTabs.js
+++ src/steps/discovery/ResultsTabs.js
@@ -1,40 +1,11 @@
 import { useState } from "react";
 import { Modal, Tabs } from "antd";
 
-import { createVennJSAdapter, VennDiagram } from "@upsetjs/react";
-import { layout } from "@upsetjs/venn.js";
-import { lab } from "d3-color";
-
 import PrimerSet from "./PrimerSet";
+import PrimerSetEulerDiagram from "./PrimerSetEulerDiagram";
 
 import { pluralize } from "../../lib/utils";
 
-
-const vennJSAdapter = createVennJSAdapter(layout);
-
-const eulerMergeColors = (colors) => {
-    // inspired by https://upset.js.org/docs/examples/vennColored
-    switch (colors.length) {
-        case 0:
-            return undefined;
-        case 1:
-            return colors[0];
-        default: {
-            const ca = colors.reduce((acc, d) => {
-                const c = lab(d || "transparent");
-                return { l: acc.l + c.l, a: acc.a + c.a, b: acc.b + c.b };
-            }, { l: 0, a: 0, b: 0 });
-
-            const res = lab(
-                ca.l / colors.length,
-                ca.a / colors.length,
-                ca.b / colors.length,
-                0.45,
-            );
-            return res.formatRgb();
-        }
-    }
-};
 
 const primerCountPhrase = (nPrimers) => `${nPrimers} ${pluralize("Primer", nPrimers)}`;
 
@@ -54,16 +25,7 @@ const ResultsTabs = ({ dataset, results, resultParams }) => {
                 style={{ top: 30 }}
                 onCancel={() => setDiagramModalVisible(false)}
             >
-                {selectedPrimerSet ? <VennDiagram
-                    layout={vennJSAdapter}
-                    sets={selectedPrimerSet.coveredTaxaByPrimerUpset}
-                    width={990}
-                    height={720}
-                    theme="light"
-                    padding={75}
-                    combinations={{ mergeColors: eulerMergeColors }}
-                    style={{ maxWidth: "100%", height: "auto" }}
-                /> : null}
+                {selectedPrimerSet ? <PrimerSetEulerDiagram primerSet={selectedPrimerSet} /> : null}
             </Modal>
             <Tabs items={results.map((res, i) => {
                 const { nPrimers, results: npResults } = res;
